e2e274cbe39622518154e5f68f841bc0c2513ade
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-03-24 17:54:58 +0300

    initial

---
+++ src/Components/Form/From.jsx
@@ -0,0 +1,8 @@
+import React from 'react' 
+import './Form.css'
+
+export default function From() {
+  return (
+    <div>From</div>
+  )
+}
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../Button/Button';
+import './Header.css'
 import { useTelegram } from '../../Hooks/useTelegram';
 
 export default function Header() {
