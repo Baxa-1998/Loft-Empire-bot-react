64adbe59c4db550a65fd4d92046ff6fb4afcc360
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-03-25 17:48:49 +0500

    product

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import './ProductList.css'
+import React from 'react';
+import './ProductList.css';
+import ProductItem from '../ProductItem/ProductItem';
 
+const product = [
+  { id: '1', title: 'Кресло', description: 'Кресла в стиле Loft', price: 1150000 },
+  { id: '2', title: 'Писменный стол', description: 'Писменные столы в стиле Loft', price: 1200000 },
+  { id: '3', title: 'Диван', description: 'Диваны в стиле Loft', price: 1800000 },
+];
 export default function ProductList() {
   return (
-    <div>ProductList</div>
-  )
+    <div className={'list'}>
+      {product.map((item) => (
+        <ProductItem product={item} onAdd={onAdd} className={'item'} />
+      ))}
+    </div>
+  );
 }
