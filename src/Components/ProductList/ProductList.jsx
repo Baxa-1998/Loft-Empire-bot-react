ceadf914933231f9a513b186e28784053af11370
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-03-26 10:47:11 +0500

    front

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -5,9 +5,21 @@ import { useTelegram } from '../../Hooks/useTelegram';
 
 const product = [
   { id: '1', img: './1.jpg', title: 'Кресло', description: 'Кресла в стиле Loft', price: 1150000 },
-  { id: '2', img: './2.jpg', title: 'Писменный стол', description: 'Писменные столы в стиле Loft', price: 1200000 },
+  {
+    id: '2',
+    img: './2.jpg',
+    title: 'Писменный стол',
+    description: 'Писменные столы в стиле Loft',
+    price: 1200000,
+  },
   { id: '3', img: './3.jpg', title: 'Диван', description: 'Диваны в стиле Loft', price: 1800000 },
-  { id: '4', img: './4.jpg', title: 'Кровать', description: 'Кровати в стиле Loft', price: 2500000 },
+  {
+    id: '4',
+    img: './4.jpg',
+    title: 'Кровать',
+    description: 'Кровати в стиле Loft',
+    price: 2500000,
+  },
 ];
 
 const getTotalPrice = (items = []) => {
@@ -18,6 +30,25 @@ const getTotalPrice = (items = []) => {
 export default function ProductList() {
   const { tg } = useTelegram();
   const [addItems, setAddItems] = React.useState([]);
+  // const onSendData = useCallback(() => {
+  //   const data = { product: addItems, totalPrice: getTotalPrice(addItems) };
+
+  //   fetch('http//localhost:8080', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  // }, []);
+
+  // React.useEffect(() => {
+  //   tg.onEvent('mainButtonClicked', onSendData);
+  //   return () => {
+  //     tg.offEvent('mainButtonClicked', onSendData);
+  //   };
+  // }, [onSendData]);
+
   const onAdd = (product) => {
     const alreadyAdded = addItems.find((item) => item.id === product.id);
     let newItems = [];
