9b698b811f2c0aa91630a379b35bf8ba11cec359
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-03-26 10:04:34 +0500

    basket

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -4,18 +4,17 @@ import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../Hooks/useTelegram';
 
 const product = [
-  { id: '1', title: 'Кресло', description: 'Кресла в стиле Loft', price: 1150000 },
-  { id: '2', title: 'Писменный стол', description: 'Писменные столы в стиле Loft', price: 1200000 },
-  { id: '3', title: 'Диван', description: 'Диваны в стиле Loft', price: 1800000 },
+  { id: '1', img: './1.jpg', title: 'Кресло', description: 'Кресла в стиле Loft', price: 1150000 },
+  { id: '2', img: './2.jpg', title: 'Писменный стол', description: 'Писменные столы в стиле Loft', price: 1200000 },
+  { id: '3', img: './3.jpg', title: 'Диван', description: 'Диваны в стиле Loft', price: 1800000 },
+  { id: '4', img: './4.jpg', title: 'Кровать', description: 'Кровати в стиле Loft', price: 2500000 },
 ];
-
-
 
 const getTotalPrice = (items = []) => {
   return items.reduce((acc, item) => {
-      return acc += item.price
-  }, 0)
-}
+    return (acc += item.price);
+  }, 0);
+};
 export default function ProductList() {
   const { tg } = useTelegram();
   const [addItems, setAddItems] = React.useState([]);
